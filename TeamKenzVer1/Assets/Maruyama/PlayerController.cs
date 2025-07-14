624df4e17f6fb65778ccec1352ab3a54614b6cf0
mkgi16cd <mkgi16cd@gmail.com>
2025-07-15 02:46:45 +0900

    [Fix] 横移動をRigidbody2DからTransform.positionを用いるように変更。

--- TeamKenzVer1/Assets/Maruyama/PlayerController.cs
+++ TeamKenzVer1/Assets/Maruyama/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D _rb;
     [SerializeField] float jumpPower = 5f;
     [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float deltaMove = 1.5f;
 
     bool _isGrounded = false;
     bool _isFacingRight = true;
@@ -19,32 +20,30 @@ public class PlayerController : MonoBehaviour
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
 
-        // 向き切り替えは入力にかかわらず一度記憶したら維持
-        if (moveInput < 0)
-        {
-            _isFacingRight = false;
-            transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
-        }
-        else if (moveInput > 0)
-        {
-            _isFacingRight = true;
-            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-        }
-
         if (_isGrounded) // 接地中
         {
             // 接地中のみ横移動
-            _rb.velocity = new Vector3(moveInput * moveSpeed, _rb.velocity.y);
+            //_rb.velocity = new Vector3(moveInput * moveSpeed, _rb.velocity.y);
+            if (moveInput > 0)
+            {
+                _isFacingRight = true;
+                transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+                transform.position += new Vector3(deltaMove * Time.deltaTime, 0, 0);
+            }
+            else if (moveInput < 0)
+            {
+                _isFacingRight = false;
+                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                transform.position -= new Vector3(deltaMove * Time.deltaTime, 0, 0);
+            }
+
 
             if (Input.GetButtonDown("Jump"))
             {
                 // ここで最後に向いてた方向でジャンプ（入力が0でもOK）
-                Vector3 jumpDirection = _isFacingRight ? new Vector3(2f, 1.5f) : new Vector3(-2f, 1.5f);
-                // Debug.Log(jumpDirection);
-                // _rb.velocity = Vector2.zero;
-                Debug.Log(jumpDirection.normalized * jumpPower);
-                //_rb.AddForce(jumpDirection.normalized * jumpPower, ForceMode2D.Impulse);
-                _rb.AddForce(new Vector2(10, 10), ForceMode2D.Impulse);
+                Vector3 jumpDirection = _isFacingRight ? new Vector3(1f, 1.8f) : new Vector3(-1f, 1.8f);
+                _rb.velocity = Vector3.zero;
+                _rb.AddForce(jumpDirection.normalized * jumpPower, ForceMode2D.Impulse);
             }
         }
     }
