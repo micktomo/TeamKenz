8a1d8d8bf41fe127ba1cf14d699c5f754966b6f4
mkgi16cd <mkgi16cd@gmail.com>
2025-07-15 20:14:59 +0900

    [Add] 溜めジャンプを実装

--- TeamKenzVer1/Assets/Maruyama/PlayerController.cs
+++ TeamKenzVer1/Assets/Maruyama/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _rb;
-    [SerializeField] float jumpPower = 5f;
+    [SerializeField] float jumpPower = 3f;
     [SerializeField] float moveSpeed = 3f;
     [SerializeField] float deltaMove = 1.5f;
+    [SerializeField] float maxChargeTime = 1f;
+    float chargeTime = 0f;
 
     bool _isGrounded = false;
     bool _isFacingRight = true;
@@ -37,13 +39,42 @@ public class PlayerController : MonoBehaviour
                 transform.position -= new Vector3(deltaMove * Time.deltaTime, 0, 0);
             }
 
-
+            /*
             if (Input.GetButtonDown("Jump"))
             {
-                // ここで最後に向いてた方向でジャンプ（入力が0でもOK）
+                // 向いている方向でジャンプ（入力なしでも transform.localScale 依存）
+                // TODO ジャンプキーの押されている時間依存でベクトルの向きと大きさを決定したい
                 Vector3 jumpDirection = _isFacingRight ? new Vector3(1f, 1.8f) : new Vector3(-1f, 1.8f);
                 _rb.velocity = Vector3.zero;
                 _rb.AddForce(jumpDirection.normalized * jumpPower, ForceMode2D.Impulse);
+            }*/
+
+            if (Input.GetButton("Jump")) // Jumpキーの入力があるとき
+            {
+                chargeTime += Time.deltaTime;
+                if (chargeTime > maxChargeTime)
+                {
+                    chargeTime = maxChargeTime;
+                }
+            }
+
+            if (Input.GetButtonUp("Jump")) // Jumpキーがリリースされたら
+            {
+                // 実際のチャージ時間と最大チャージ時間を0～1で返す
+                float chargeGage = Mathf.Clamp01(chargeTime / maxChargeTime);
+                // 上記を用いて角度を調整（ここでは20～70）
+                float radValue = Mathf.Lerp(20f, 70f, chargeGage);
+                // 度数法（Rad）として変換
+                float rad = radValue * Mathf.Deg2Rad;
+                // ジャンプの大きさも同様に調整
+                float power = Mathf.Lerp(jumpPower * 0.5f, jumpPower, chargeGage);
+                float dirX = Mathf.Cos(rad) * (_isFacingRight ? 1 : -1);
+                float dirY = Mathf.Sin(rad);
+                Vector3 jumpDirection = new Vector3(dirX, dirY, 0f);
+                _rb.velocity = Vector3.zero;
+                _rb.AddForce(jumpDirection.normalized * power, ForceMode2D.Impulse);
+
+                chargeTime = 0; // チャージタイムリセット
             }
         }
     }
