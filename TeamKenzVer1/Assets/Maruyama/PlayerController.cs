f3348d79b94e3d60149ad8f4c9992a84249020c9
mkgi16cd <mkgi16cd@gmail.com>
2025-07-16 23:38:48 +0900

    Main Scene調整

--- TeamKenzVer1/Assets/Maruyama/PlayerController.cs
+++ TeamKenzVer1/Assets/Maruyama/PlayerController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _rb;
-    [SerializeField] float jumpPower = 3f;
-    [SerializeField] float moveSpeed = 3f;
-    [SerializeField] float deltaMove = 1.5f;
-    [SerializeField] float maxChargeTime = 1f;
-    float chargeTime = 0f;
-
+    [SerializeField] float _jumpPower = 15f;
+    [SerializeField] float _deltaMove = 1.5f;
+    [SerializeField] float _maxChargeTime = 1f;
+    [SerializeField] float _minAngle = 25f;
+    [SerializeField] float _maxAngle = 70f;
+    float _chargeTime = 0f;
     bool _isGrounded = false;
     bool _isFacingRight = true;
 
@@ -22,61 +22,53 @@ public class PlayerController : MonoBehaviour
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
 
-        if (_isGrounded) // 接地中
+        if (_isGrounded)
         {
-            // 接地中のみ横移動
-            //_rb.velocity = new Vector3(moveInput * moveSpeed, _rb.velocity.y);
             if (moveInput > 0)
             {
                 _isFacingRight = true;
                 transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-                transform.position += new Vector3(deltaMove * Time.deltaTime, 0, 0);
+                transform.position += new Vector3(_deltaMove * Time.deltaTime, 0, 0);
             }
             else if (moveInput < 0)
             {
                 _isFacingRight = false;
                 transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
-                transform.position -= new Vector3(deltaMove * Time.deltaTime, 0, 0);
+                transform.position -= new Vector3(_deltaMove * Time.deltaTime, 0, 0);
             }
 
-            /*
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetButton("Jump"))
             {
-                // 向いている方向でジャンプ（入力なしでも transform.localScale 依存）
-                // TODO ジャンプキーの押されている時間依存でベクトルの向きと大きさを決定したい
-                Vector3 jumpDirection = _isFacingRight ? new Vector3(1f, 1.8f) : new Vector3(-1f, 1.8f);
-                _rb.velocity = Vector3.zero;
-                _rb.AddForce(jumpDirection.normalized * jumpPower, ForceMode2D.Impulse);
-            }*/
-
-            if (Input.GetButton("Jump")) // Jumpキーの入力があるとき
-            {
-                chargeTime += Time.deltaTime;
-                if (chargeTime > maxChargeTime)
+                _chargeTime += Time.deltaTime;
+                if (_chargeTime > _maxChargeTime)
                 {
-                    chargeTime = maxChargeTime;
+                    _chargeTime = _maxChargeTime;
                 }
             }
 
-            if (Input.GetButtonUp("Jump")) // Jumpキーがリリースされたら
+            if (Input.GetButtonUp("Jump"))
             {
-                // 実際のチャージ時間と最大チャージ時間を0～1で返す
-                float chargeGage = Mathf.Clamp01(chargeTime / maxChargeTime);
-                // 上記を用いて角度を調整（ここでは20～70）
-                float radValue = Mathf.Lerp(25f, 70f, chargeGage);
-                // 度数法（Rad）として変換
-                float rad = radValue * Mathf.Deg2Rad;
-                // ジャンプの大きさも同様に調整
-                float power = Mathf.Lerp(jumpPower * 0.5f, jumpPower, chargeGage);
-                float dirX = Mathf.Cos(rad) * (_isFacingRight ? 1 : -1);
-                float dirY = Mathf.Sin(rad);
-                Vector3 jumpDirection = new Vector3(dirX, dirY, 0f);
-                _rb.velocity = Vector3.zero;
-                _rb.AddForce(jumpDirection.normalized * power, ForceMode2D.Impulse);
-
-                chargeTime = 0; // チャージタイムリセット
+                Jump(_jumpPower, _minAngle, _maxAngle);
             }
         }
+    }
+
+    void Jump(float jumpPower, float minAngle, float maxAngle)
+    {
+        // チャージ時間と最大チャージ時間を0～1で返す
+        float chargeGage = Mathf.Clamp01(_chargeTime / _maxChargeTime);
+        float radValue = Mathf.Lerp(minAngle, maxAngle, chargeGage);
+        float rad = radValue * Mathf.Deg2Rad;
+
+        float power = Mathf.Lerp(jumpPower * 0.5f, jumpPower, chargeGage);
+        float dirX = Mathf.Cos(rad) * (_isFacingRight ? 1 : -1);
+        float dirY = Mathf.Sin(rad);
+
+        Vector3 jumpDirection = new Vector3(dirX, dirY, 0f);
+        _rb.velocity = Vector3.zero;
+        _rb.AddForce(jumpDirection.normalized * power, ForceMode2D.Impulse);
+
+        _chargeTime = 0;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
